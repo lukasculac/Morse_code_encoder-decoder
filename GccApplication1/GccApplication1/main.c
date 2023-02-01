f14e4cab1a7bd6de4c4d1c5fe04e5df51c969c9c
PMedur <93006016+PMedur@users.noreply.github.com>
2023-02-01 20:44:42 +0100

    Add files via upload

--- GccApplication1/GccApplication1/main.c
+++ GccApplication1/GccApplication1/main.c
@@ -6,20 +6,30 @@
 #include <avr/io.h>
 #include <stdlib.h>
 
-ISR(USART_RXC_vect) {
-	
-	PORTA = _BV(0);
+ISR(USARTRXC_vect) {
+	PORTA |= 0x01;
 }
 
 
+unsigned char USART_getc( void )
+{
+	// Wait for data to be received
+	while ( !(UCSRA & _BV(RXC)) );
+	// Get and return received data from buffer
+	PORTA |= 0x01;
+	return 0;
+}
+
 void UART_Init(uint16_t v_baudRate_u16)
 {
-	UCSRA= _BV(U2X);                 // Clear the UASRT status register
-	UCSRB= (1<<RXEN) | (1<<RXCIE);     // Enable Receiver and Transmitter
+	
+	UCSRA= 0x00;                 // Clear the UASRT status register
+	UCSRB= (1<<RXEN) | (1<<RXCIE) | (1 << TXEN);     // Enable Receiver and Transmitter
 	UCSRC= (1<<UCSZ1) | (1<<UCSZ0);   // Async-mode
 	
 	UBRRL = v_baudRate_u16;
-	UBRRH = v_baudRate_u16;
+	UBRRH = (v_baudRate_u16 >> 8);
+	PORTA |= 0x04;
 }
 
 
@@ -27,11 +37,12 @@ int main(void)
 {
 	
 	DDRA = 0xff;
-	PORTA = 0xff;
-	UART_Init(95);
-	
+	PORTA = 0x00;
+	UART_Init(3);
+	sei();
     while (1) 
     {
+		USART_getc();
     }
 }
 
